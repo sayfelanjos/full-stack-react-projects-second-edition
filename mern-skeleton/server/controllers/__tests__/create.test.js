604c4cc01637d7e98b1afe62e005005c8dcd00a0
CaioAANolasco <caioaanolasco@gmail.com>
2022-06-29 10:38:56 -0300

    Set up mongodb connection opening and closng for create test

--- mern-skeleton/server/controllers/__tests__/create.test.js
+++ mern-skeleton/server/controllers/__tests__/create.test.js
@@ -1,3 +1,7 @@
+import userController from "../user.controller";
+import config from "../../../config/config";
+import mongoose from 'mongoose'
+
 const mockRequest = (body) => (
 	{
 		body
@@ -10,3 +14,17 @@ const mockResponse = () => {
 	res.json = jest.fn().mockReturnValue(res);
 	return res;
 };
+
+describe('create user', () => {
+	beforeAll(async () => {
+		mongoose.Promise = global.Promise
+		mongoose.connect(config.mongoUri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+		mongoose.connection.on('error', () => {
+			throw new Error(`unable to connect to database: ${config.mongoUri}`)
+		})
+	});
+
+	afterAll(async () => {
+		await mongoose.connection.close();
+	});
+});
