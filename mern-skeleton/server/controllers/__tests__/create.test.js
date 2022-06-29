1a2d9310635ffca39908d4102685ea87d5d0cb38
CaioAANolasco <caioaanolasco@gmail.com>
2022-06-29 10:34:25 -0300

    Created test file with mock req and res

---
+++ mern-skeleton/server/controllers/__tests__/create.test.js
@@ -0,0 +1,12 @@
+const mockRequest = (body) => (
+	{
+		body
+	}
+);
+
+const mockResponse = () => {
+	const res = {};
+	res.status = jest.fn().mockReturnValue(res);
+	res.json = jest.fn().mockReturnValue(res);
+	return res;
+};
